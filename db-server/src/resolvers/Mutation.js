2298a88c8a178f25b11a78271d71f83f6bbd1576
nursh <nsheikhmohammed@yahoo.com>
2018-11-11 20:10:20 -0500

    Added create, update and delete mutations for menuItem

--- db-server/src/resolvers/Mutation.js
+++ db-server/src/resolvers/Mutation.js
@@ -1,14 +1,22 @@
 const Mutation = {
   async createMenuInput(parent, args, { prisma }, info) {
-    const menuItem = await prisma.createmenuItem({
+    const menuItem = await prisma.createMenuItem({
       name: args.data.name,
       price: args.data.price,
       category: args.data.category
     });
     return menuItem;
   },
+  async updateMenuInput(parent, args, { prisma }, info) {
+    const menuItem = await prisma.updateMenuItem({
+      where: {
+        id: args.id
+      },
+      data: args.data
+    });
+  },
   async deleteMenuInput(parent, args, { prisma }, info) {
-    const menuItem = await prisma.deletemenuItem({ id: args.id });
+    const menuItem = await prisma.deleteMenuItem({ id: args.id });
     return menuItem;
   }
 };
