b74ef78b1bd8a746d9c18f95f568bcb67492c2ba
nursh <nsheikhmohammed@yahoo.com>
2018-11-13 14:53:40 -0500

    Refactored banner-heading

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,17 +6,18 @@ class Home extends Component {
     return (
       <div className="app">
         <div className="banner">
-          <div className="banner__title">Welcome to</div>
-          <div className="banner__body">African restaurant</div>
+          <h1 className="banner-heading">
+            <span className="banner-heading__welcome">Welcome to</span>
+            <span className="banner-heading__primary">Ruby Continental | Restaurant</span>
+          </h1>
+
           <div className="banner__text">
             Hello fellow food lovers, Try our delicious african cuisine and have
             your taste buds tickled
           </div>
-          <div>
-            <Link to="/menu" className="banner__button">
-              Browse our Menu
-            </Link>
-          </div>
+          <Link to="/menu" className="banner__button">
+            Browse our Menu
+          </Link>
         </div>
       </div>
     );
