4fa81e971bf4ab6c0006b549fcc3fa7f7a30ddda
nursh <nsheikhmohammed@yahoo.com>
2018-11-14 09:28:15 -0500

    Added grid to display menu-category and css for menu-category

--- client/src/components/MenuCategories.js
+++ client/src/components/MenuCategories.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
-import MenuCategory from './MenuCategory';
+import MenuCategory from "./MenuCategory";
 
 class MenuCategories extends Component {
   render() {
     return (
       <div className="row">
-        <MenuCategory />
+        <div className="col-1-of-3">
+          <MenuCategory text="Drinks"/>
+        </div>
+        <div className="col-1-of-3">
+          <MenuCategory text="Soups"/>
+        </div>
+        <div className="col-1-of-3">
+          <MenuCategory text="Breakfast"/>
+        </div>
       </div>
     );
   }
--- client/src/components/MenuCategory.js
+++ client/src/components/MenuCategory.js
@@ -4,7 +4,12 @@ class MenuCategory extends Component {
   render() {
     return (
       <div className="menu-category">
-        <h2 className="menu-category__title">African Hot Pot</h2>
+        <h2 className="menu-category__title">
+          <span className="menu-category__title-text">{this.props.text}</span>
+        </h2>
+        <div className="menu-category__circle">
+          <span className="menu-category__circle-arrow">&#10148;</span>
+        </div>
       </div>
     );
   }
