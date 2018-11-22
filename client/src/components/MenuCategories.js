05159516e142d9526aa81878893a34fb11df0ff0
nursh <nsheikhmohammed@yahoo.com>
2018-11-22 04:00:17 -0500

    Added apollo-client to project

--- client/src/components/MenuCategories.js
+++ client/src/components/MenuCategories.js
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import MenuCategory from "./MenuCategory";
 import Header from './Header';
 
 class MenuCategories extends Component {
   render() {
+    console.log(this.props);
     return (
-      <div>
-        <Header />
-        <div className="row">
-          <div className="col-1-of-3">
-            <a href="/menu/drinks">
-              <MenuCategory text="Drinks"/>
-            </a>
-          </div>
-          <div className="col-1-of-3">
-            <MenuCategory text="Soups"/>
-          </div>
-          <div className="col-1-of-3">
-            <MenuCategory text="Breakfast"/>
+        <div>
+          <Header />
+          <div className="row">
+            <div className="col-1-of-3">
+              <a href="/menu/drinks">
+                <MenuCategory text="Drinks"/>
+              </a>
+            </div>
+            <div className="col-1-of-3">
+              <MenuCategory text="Soups"/>
+            </div>
+            <div className="col-1-of-3">
+              <MenuCategory text="Breakfast"/>
+            </div>
           </div>
         </div>
-      </div>
     );
   }
 }
 
-export default MenuCategories;
+const GET_CATEGORIES = gql`
+  query {
+    menuItems {
+      category
+    }
+  }
+`;
+
+export default graphql(GET_CATEGORIES)(MenuCategories);
