958929fe63cb1beb82008424b5f9a8a512fcb16a
nursh <nsheikhmohammed@yahoo.com>
2018-11-29 15:39:43 -0500

    Added Proceed to checkout button

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -1,11 +1,52 @@
-import React, { Component } from 'react';
-import gql from 'graphql-tag';
-import { graphql, compose } from 'react-apollo';
-import Header from './Header';
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql, compose } from "react-apollo";
+import update from "immutability-helper";
+import Header from "./Header";
 
 class Order extends Component {
+  menuState = {
+    items: []
+  };
 
-  removeFromOrder = (itemId) => async (e) => {
+  addQuantity = order => e => {
+    const itemIndex = this.menuState.items.findIndex(
+      item => item.name === order.name
+    );
+    const newState = update(this.menuState, {
+      items: {
+        [itemIndex]: {
+          quantity: {
+            $apply: x => (x += 1)
+          }
+        }
+      }
+    });
+    this.menuState = newState;
+    this.forceUpdate();
+  };
+
+  reduceQuantity = order => e => {
+    const itemIndex = this.menuState.items.findIndex(
+      item => item.name === order.name
+    );
+    const newState = update(this.menuState, {
+      items: {
+        [itemIndex]: {
+          quantity: {
+            $apply: x => {
+              x -= 1;
+              return x < 1 ? 0 : x;
+            }
+          }
+        }
+      }
+    });
+    this.menuState = newState;
+    this.forceUpdate();
+  };
+
+  removeFromOrder = itemId => async e => {
     await this.props.removeItem({
       variables: {
         id: itemId
@@ -13,64 +54,79 @@ class Order extends Component {
     });
     this.props.data.refetch();
     this.forceUpdate();
-  }
+  };
 
-  renderOrderItems = (orderItems) => {
+  renderOrderItems = orderItems => {
     const rows = orderItems.map(item => (
       <tr className="menu-table__body__row" key={item.name}>
         <td className="menu-table__body-name">{item.name}</td>
         <td className="menu-table__body-quantity">
           <button className="menu-table__body-button">-</button>
-          <span className="menu-table__body-quantity__number">{item.quantity}</span>
+          <span className="menu-table__body-quantity__number">
+            {item.quantity}
+          </span>
           <button className="menu-table__body-button">+</button>
         </td>
-        <td className="menu-table__body-price">&#8358;{item.price * item.quantity}</td>
+        <td className="menu-table__body-price">
+          &#8358;{item.price * item.quantity}
+        </td>
         <td className="menu-table__body-order">
-          <button className="menu-table__body-order__button" onClick={this.removeFromOrder(item.id)}>
+          <button
+            className="menu-table__body-order__button"
+            onClick={this.removeFromOrder(item.id)}
+          >
             Remove item(s)
           </button>
         </td>
       </tr>
-      )
-    );
+    ));
 
     return rows;
-  }
-
+  };
 
   render() {
     const { order } = this.props.data;
     const { error } = this.props.data;
-    if (error) return 'An order does not exist yet';
-    if (!order) return 'Loading...';
+    if (!order) return "Loading...";
     return (
       <div>
-      <Header />
-      <h2 className="order__title">Review Order</h2>
-      <table className="menu-table">
-        <thead>
-          <tr>
-            <th className="menu-table__heading-items">ITEMS</th>
-            <th className="menu-table__heading-quantity">QUANTITY</th>
-            <th className="menu-table__heading-amount">AMOUNT</th>
-            <th className="menu-table__heading-order"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.renderOrderItems(order.items)}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td></td>
-            <td className="menu-table__body-total">Total</td>
-            <td className="menu-table__body-price">
-              &#8358;{order.items.reduce((sum, item) => sum += (item.price * item.quantity), 0)}
-            </td>
-            <td></td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
+        <Header />
+        <h2 className="order__title">Review Order</h2>
+        {error || order.items.length === 0 ? (
+          <h2 className="order__error">
+            No item(s) have been added to the order
+          </h2>
+        ) : (
+          <div>
+            <table className="menu-table">
+              <thead>
+                <tr>
+                  <th className="menu-table__heading-items">ITEMS</th>
+                  <th className="menu-table__heading-quantity">QUANTITY</th>
+                  <th className="menu-table__heading-amount">AMOUNT</th>
+                  <th className="menu-table__heading-order" />
+                </tr>
+              </thead>
+              <tbody>{this.renderOrderItems(order.items)}</tbody>
+              <tfoot>
+                <tr>
+                  <td />
+                  <td className="menu-table__body-total">Total</td>
+                  <td className="menu-table__body-price">
+                    &#8358;
+                    {order.items.reduce(
+                      (sum, item) => (sum += item.price * item.quantity),
+                      0
+                    )}
+                  </td>
+                  <td />
+                </tr>
+              </tfoot>
+            </table>
+            <button className="order__checkout">Continue to checkout</button>
+          </div>
+        )}
+      </div>
     );
   }
 }
@@ -97,6 +153,14 @@ const removeOrderItem = gql`
   }
 `;
 
+const updateOrder = gql`
+  mutation($id: ID!, $order: ID!, $quantity: Int) {
+    updateOrderItem(id: $id, data: { order: $order, quantity: $quantity }) {
+      id
+    }
+  }
+`;
+
 const orderId = window.localStorage.getItem("orderId");
 export default compose(
   graphql(fetchOrders, {
@@ -106,5 +170,6 @@ export default compose(
       }
     })
   }),
-  graphql(removeOrderItem, { name: 'removeItem' })
+  graphql(removeOrderItem, { name: "removeItem" }),
+  graphql(updateOrder, { name: "updateOrder" })
 )(Order);
