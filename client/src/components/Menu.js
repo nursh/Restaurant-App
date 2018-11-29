f3de9d137445a40d8f84fd38ce33507618cb5ebc
nursh <nsheikhmohammed@yahoo.com>
2018-11-29 02:29:07 -0500

    Finished with adding and reducing quantity and adding menu item to order

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,12 +1,55 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
 import { graphql, compose } from "react-apollo";
+import update from "immutability-helper";
 import Header from "./Header";
 
 const getOrderId = () => window.localStorage.getItem("orderId");
 
 class Menu extends Component {
-  addToOrder = name => async e => {
+  menuState = {
+    items: []
+  };
+
+  addQuantity = order => e => {
+    const itemIndex = this.menuState.items.findIndex(
+      item => item.name === order.name
+    );
+    const newState = update(this.menuState, {
+      items: {
+        [itemIndex]: {
+          quantity: {
+            $apply: x => (x += 1)
+          }
+        }
+      }
+    });
+    this.menuState = newState;
+    this.forceUpdate();
+  };
+
+  reduceQuantity = order => e => {
+    const itemIndex = this.menuState.items.findIndex(
+      item => item.name === order.name
+    );
+    const newState = update(this.menuState, {
+      items: {
+        [itemIndex]: {
+          quantity: {
+            $apply: x => {
+              x -= 1;
+              return x < 1 ? 0 : x;
+            }
+          }
+        }
+      }
+    });
+    this.menuState = newState;
+    this.forceUpdate();
+  };
+
+  addToOrder = item => async e => {
+    const { name, quantity, price } = item;
     let orderId = getOrderId();
     let createdOrder;
     if (!orderId) {
@@ -19,27 +62,50 @@ class Menu extends Component {
     await this.props.addOrderItem({
       variables: {
         name,
-        quantity: 2,
-        price: 2.0,
+        quantity,
+        price,
         order: orderId
       }
     });
   };
 
+  addItemToState = ({ name, price }) => {
+    const orderItem = {
+      name,
+      price,
+      quantity: 0
+    };
+    this.menuState.items.push(orderItem);
+    return orderItem;
+  };
+
   renderMenuItem = menuItems => {
-    const itemRows = menuItems.map(({ name, price }) => (
-      <tr className="menu-table__body__row" key={name}>
-        <td className="menu-table__body-name">{name}</td>
+
+    const itemRows = menuItems.map(item => (
+      <tr className="menu-table__body__row" key={item.name}>
+        <td className="menu-table__body-name">{item.name}</td>
         <td className="menu-table__body-quantity">
-          <button className="menu-table__body-button">-</button>
-          <span className="menu-table__body-quantity__number">0</span>
-          <button className="menu-table__body-button">+</button>
+          <button
+            className="menu-table__body-button"
+            onClick={this.reduceQuantity(item)}
+          >
+            -
+          </button>
+          <span className="menu-table__body-quantity__number">
+            {item.quantity}
+          </span>
+          <button
+            className="menu-table__body-button"
+            onClick={this.addQuantity(item)}
+          >
+            +
+          </button>
         </td>
-        <td className="menu-table__body-price">&#8358;{price}</td>
+        <td className="menu-table__body-price">&#8358;{item.price}</td>
         <td className="menu-table__body-order">
           <button
             className="menu-table__body-order__button"
-            onClick={this.addToOrder(name)}
+            onClick={this.addToOrder(item)}
           >
             Add to Order
           </button>
@@ -51,7 +117,13 @@ class Menu extends Component {
 
   render() {
     const { menuItems } = this.props.data;
+    let menuRows;
     if (!menuItems) return "Loading...";
+    if (this.menuState.items.length !== 0) {
+      menuRows = this.menuState.items;
+    } else {
+      menuRows = menuItems.map(item => this.addItemToState(item));
+    }
     return (
       <div>
         <Header />
@@ -64,7 +136,7 @@ class Menu extends Component {
               <th className="menu-table__heading-order" />
             </tr>
           </thead>
-          <tbody>{this.renderMenuItem(menuItems)}</tbody>
+          <tbody>{this.renderMenuItem(menuRows)}</tbody>
         </table>
       </div>
     );
